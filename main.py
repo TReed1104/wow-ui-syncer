307da65963d5bc35317dab476083931c61a7dc9e
Tom Reed <t.reed1104@gmail.com>
2022-01-09 17:11:57 +0000

    Renamed the folder arg to target

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     ## Setup the command-line arguments
     argParser = ArgumentParser(description='A Python app for syncing my WoW Addons and UI folder between machines')
     argParser.add_argument("-m", "--mode", dest="mode", choices=["pull", "push"], help="The sync mode, push or pull", type=str, default="pull")
-    argParser.add_argument("-f", "--folder", dest="folder", choices=["UI", "Addons"], help="The folder to sync", type=str, default="ui")
+    argParser.add_argument("-f", "--target", dest="target", choices=["UI", "Addons"], help="The folder to sync", type=str, default="ui")
 
     ## Parse the arguments from the command line to be usable
     args = argParser.parse_args()
@@ -23,26 +23,26 @@ if __name__ == '__main__':
     ## Check the Sync mode - Pull/Push
     if args.mode == "pull":
         ## Check which folder we're Syncing
-        if args.folder == "UI":
+        if args.target == "UI":
             ## Pull the UI Folder
-            pullFolder(args.folder)
+            pullFolder(args.target)
 
-        elif args.folder == "Addons":
+        elif args.target == "Addons":
             ## Pull the Addons Folder
-            pullFolder(args.folder)
+            pullFolder(args.target)
 
         else:
             print("Pulling Unknown Folder")
 
     elif args.mode == "push":
         ## Check which folder we're Syncing
-        if args.folder == "UI":
+        if args.target == "UI":
             ## Push the UI Folder
-            pushFolder(args.folder)
+            pushFolder(args.target)
 
-        elif args.folder == "Addons":
+        elif args.target == "Addons":
             ## Push the Addons Folder
-            pushFolder(args.folder)
+            pushFolder(args.target)
 
         else:
             print("Pushing Unknown Folder")
