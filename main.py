4ec12caad6d31f67140a50565422a8c97cf55109
Tom Reed <t.reed1104@gmail.com>
2022-01-09 16:48:04 +0000

    Setup the cli args

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from argparse import ArgumentParser
 
 ## Main thread check
 if __name__ == '__main__':
-    print("Hello World")
+    ## Setup the command-line arguments
+    argParser = ArgumentParser(description='A Python app for syncing my WoW Addons and UI folder between machines')
+    argParser.add_argument("-m", "--mode", dest="mode", choices=["pull", "push"], help="The sync mode, push or pull", type=str, default="pull")
+    argParser.add_argument("-f", "--folder", dest="folder", choices=["ui", "addons"], help="The folder to sync", type=str, default="ui")
+
+    ## Parse the arguments from the command line to be usable
+    args = argParser.parse_args()
+
+    ## Entry point output, just for context in the command-line
+    print("Syncing WoW Folders")
+    
